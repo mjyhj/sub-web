726b7361433fa8f4c550496be2d7b7d200ee56fd
mjyhj <22577418+mjyhj@users.noreply.github.com>
2021-03-03 09:52:34 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -256,7 +256,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-	  "sub.mjyhj.win (mj）": "https://sub.mjyhj.win/sub?",
+          "sub.mjyhj.win(mj）": "https://sub.mjyhj.win/sub?",
           "sub-beta.now.sh (自动编译最新版本后端-测试）": "https://sub-beta.now.sh/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
@@ -265,7 +265,7 @@ export default {
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
-	  { value: "https://sub.mjyhj.win/sub?" },
+          { value: "https://sub.mjyhj.win/sub?" },
           { value: "https://sub-beta.now.sh/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
